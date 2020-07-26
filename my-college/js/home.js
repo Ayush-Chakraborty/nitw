1a7f3c608b2f88d2049018ce6cbe4064d1f4c91f
Ayush-Chakraborty <61818015+Ayush-Chakraborty@users.noreply.github.com>
2020-07-26 23:21:28 +0530

    Update home.js

--- my-college/js/home.js
+++ my-college/js/home.js
@@ -3,6 +3,8 @@ var on=true;
 var i=0;
 var timer=null;
 function rotate(){
+    if(i>10**10)
+        i=0;
     i++;
     mainImage.setAttribute("style",`transform:rotateY(${i}deg);`);
 }
@@ -21,5 +23,34 @@ function change_state(){
         on=true;
     }
 }
+var header=document.getElementsByTagName("header")[0];
+function change_index(){
+    if(window.scrollY>0)
+        header.style.zIndex='1';
+    else
+        header.style.zIndex='0';
+}
+window.addEventListener("scroll",change_index);
 
-// var timer=setInterval(rotate,50);
+var sections=document.getElementsByClassName('between_section');
+function change_visibility(){
+
+    for(var i=0;i<sections.length;i++){
+        if (window.innerHeight+window.scrollY>sections[i].offsetTop+200){
+            sections[i].setAttribute("style","transform:translateY(0px)");
+            sections[i].style.opacity = '1';
+        }
+        else{
+            sections[i].setAttribute("style","transform:translateY(100px)");
+            sections[i].style.opacity = '0';
+        }
+    }
+}
+window.addEventListener("scroll",change_visibility);
+function scrollToTop(){
+    window.scroll({
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth'
+    });
+}
